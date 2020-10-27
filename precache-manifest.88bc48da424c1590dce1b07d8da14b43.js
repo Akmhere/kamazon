668cf3daa523fe92bffc7ea638f51b554f7cb1cb
Akmhere <itzakm33@gmail.com>
2020-10-27 15:16:13 +0530

    Updates

---
+++ precache-manifest.88bc48da424c1590dce1b07d8da14b43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2df9ac5776c857e4be2cacccb9f2de",
+    "url": "/kamazon/index.html"
+  },
+  {
+    "revision": "229262b3bc82fcfa3638",
+    "url": "/kamazon/static/css/main.3abbd6a0.chunk.css"
+  },
+  {
+    "revision": "648f67e73786b28b6e40",
+    "url": "/kamazon/static/js/2.865d18e8.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/kamazon/static/js/2.865d18e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229262b3bc82fcfa3638",
+    "url": "/kamazon/static/js/main.cddc5e40.chunk.js"
+  },
+  {
+    "revision": "9e60e49ca8eb66ab3f21",
+    "url": "/kamazon/static/js/runtime-main.1f2a0ac4.js"
+  }
+]);
